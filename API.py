6ee6c19c8539ccc290f217dbda0de3b5d6257930
IamSerJanGO <130689026+IamSerJanGO@users.noreply.github.com>
2023-10-30 16:54:55 +0200

    Update API.py

--- API.py
+++ API.py
@@ -1,7 +1,7 @@
 import aiogram
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-TOKEN_API = '6712465987:AAEY7cCPbp8BhcBjtzcf1zc1m6K1XRLHE54'  # Авторизационный токен
+TOKEN_API = ''  # Авторизационный токен
 bot = aiogram.Bot(TOKEN_API)
 dp = aiogram.Dispatcher()
 
